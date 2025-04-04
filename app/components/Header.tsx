aa609588df8e9ad158aa1bd1f6d0d4f15c1e6823
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-04-04 16:20:36 +0900

    root.tsxの構成を修正

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,35 +2,33 @@ import { Link } from "@remix-run/react";
 
 export default function Header({ userId }: { userId: string | null }) {
   return (
-    <div>
-      <header>
-        <Link to="/">Remix-Tech-Article</Link>
-        <nav>
-          <Link to="/">TOP</Link>
-        </nav>
-        {userId ? (
-          <div>
-            <nav>
-              <Link to="/write">記事作成</Link>
-            </nav>
-            <nav>
-              <Link to="/mypage">マイページ</Link>
-            </nav>
-            <nav>
-              <Link to="/logout">ログアウト</Link>
-            </nav>
-          </div>
-        ) : (
-          <div>
-            <nav>
-              <Link to="/login">ログイン</Link>
-            </nav>
-            <nav>
-              <Link to="/signup">サインアップ</Link>
-            </nav>
-          </div>
-        )}
-      </header>
-    </div>
+    <header>
+      <Link to="/">Remix-Tech-Article</Link>
+      <nav>
+        <Link to="/">TOP</Link>
+      </nav>
+      {userId ? (
+        <div>
+          <nav>
+            <Link to="/write">記事作成</Link>
+          </nav>
+          <nav>
+            <Link to="/mypage">マイページ</Link>
+          </nav>
+          <nav>
+            <Link to="/logout">ログアウト</Link>
+          </nav>
+        </div>
+      ) : (
+        <div>
+          <nav>
+            <Link to="/login">ログイン</Link>
+          </nav>
+          <nav>
+            <Link to="/signup">サインアップ</Link>
+          </nav>
+        </div>
+      )}
+    </header>
   );
 }
