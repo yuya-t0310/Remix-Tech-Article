115b59eb5cb864676e5acf74e554dd9f2dfe8a59
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-04-11 15:48:04 +0900

    記事作成ページをmarkdownに対応

---
+++ app/components/ArticleEditor.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import remarkGfm from "remark-gfm";
+import { Form } from "@remix-run/react";
+
+function ArticleForm() {
+  const [markdown, setMarkDown] = useState<string>("");
+
+  return (
+    <Form id="article-form" method="post">
+      <div className="m-2">
+        <div className="font-light">タイトル</div>
+        <input
+          name="title"
+          type="text"
+          aria-label="Title"
+          placeholder="タイトル"
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#7ec7d8] focus:border-[#7ec7d8] outline-none transition-colors"
+        />
+      </div>
+
+      <div className="m-2">
+        <div className="font-light">内容</div>
+        <textarea
+          value={markdown}
+          onChange={(e) => setMarkDown(e.target.value)}
+          aria-label="content"
+          placeholder="Markdown記法が使用可能です。"
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#7ec7d8] focus:border-[#7ec7d8] outline-none transition-colors"
+        />
+      </div>
+
+      <div className="m-2">
+        <div className="font-light">プレビュー</div>
+        <div className="prose markdown-body max-w-none h-72 px-4 py-2 border border-gray-300 rounded-lg bg-white overflow-y-auto">
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeRaw]}
+          >
+            {markdown}
+          </ReactMarkdown>
+        </div>
+        <input type="hidden" name="content" value={markdown} />
+      </div>
+
+      <div className="m-2">
+        <button
+          type="submit"
+          className="w-full bg-[#7ec7d8]/75 text-white font-medium py-2 px-4 rounded-lg hover:bg-[#7ec7d8] transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+        >
+          投稿
+        </button>
+      </div>
+    </Form>
+  );
+}
+
+export default ArticleForm;
--- app/routes/write.tsx
+++ app/routes/write.tsx
@@ -1,8 +1,8 @@
 import { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
-import { Form } from "@remix-run/react";
 import prisma from "../../lib/prisma";
 import { requireUserSession } from "../data/auth.server";
 import { setFlashMessage } from "../utils/session";
+import ArticleForm from "../components/ArticleEditor";
 
 // ログイン状態でなければトップページへリダイレクト
 export const loader = async ({ request }: LoaderFunctionArgs) => {
@@ -38,25 +38,8 @@ export default function WriteArticle() {
   return (
     <>
       <div className="text-xl font-bold">記事作成</div>
-      <div>
-        <Form id="article-form" method="post">
-          <p>
-            <span>タイトル</span>
-            <input
-              name="title"
-              type="text"
-              aria-label="Title"
-              placeholder="タイトル"
-            />
-          </p>
-          <p>
-            <span>内容</span>
-            <textarea name="content" rows={12} placeholder="本文"></textarea>
-          </p>
-          <p>
-            <button type="submit">投稿</button>
-          </p>
-        </Form>
+      <div className="m-4">
+        <ArticleForm />
       </div>
     </>
   );
