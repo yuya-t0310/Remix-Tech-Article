d2ba51fd50e61281722086e21d077bc7b70e4dc6
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-04-17 16:25:49 +0900

    タイトルと著者名をtruncateに変更

--- app/components/ArticleCard.tsx
+++ app/components/ArticleCard.tsx
@@ -16,14 +16,14 @@ type Article = {
 export default function ArticleCard({ article }: { article: Article }) {
   return (
     <>
-      <div className="shadow-md max-w-96 p-4 bg-[#7ec7d8]/25">
+      <div className="shadow-md max-w-96 p-4 bg-[#7ec7d8]/25 rounded-md">
         <Link to={`/article/${article.id}`}>
-          <div className="text-xl line-clamp-1 hover:underline underline-offset-1 decoration-2">
+          <div className="text-xl w-90 truncate hover:underline underline-offset-1 decoration-2">
             {article.title}
           </div>
         </Link>
         <div className="space-x-4 flex items-center">
-          <span className="text-sm font-light hover:underline underline-offset-1 decoration-1">
+          <span className="text-sm w-30 truncate font-light hover:underline underline-offset-1 decoration-1">
             {article.author.profile?.name}
           </span>
           <span className="text-xs font-thin">
